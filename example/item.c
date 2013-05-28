47a842f37d61bfaf4d700646e40a99e83863905d
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-27 23:24:14 -0700

    added lambda example

--- example/item.c
+++ example/item.c
@@ -1,11 +1,32 @@
 #include "item.h"
 
+item half(item i) 
+{  
+  i->value = i->value * .5;
+  return i;
+}
+
+item print(item i) 
+{  
+  printf("%i, ", i->value);
+  return i;
+}
+
 int main(int argc, char *argv[])
 {
   itemDequeue ideq = new_itemDequeue();
-  item i = new_item();
-  i->value = 4;
-  itemDequeue_add(ideq, i);
-  itemDequeue_print(ideq);
-  return 1;
+  int i;
+  for (i=0; i<10; i++) {
+    item tmp = new_item();
+    tmp->value = rand();
+    itemDequeue_add(ideq, tmp);
+  }
+  printf("[");
+  ideq = itemDequeue_map(ideq, &print);
+  printf("...],\n");
+  ideq = itemDequeue_map(ideq, &half);
+  printf("[");
+  ideq = itemDequeue_map(ideq, &print);
+  printf("...]\n");
+  return 0;
 }
